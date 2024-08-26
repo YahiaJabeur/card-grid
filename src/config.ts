0aad9a5be0254440b3e94e29c5e73cfb55d204ef
YahiaJabeur <yahiajabeur@gmail.com>
2024-08-26 08:07:05 +0100

    chore: create cinfig file

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const LIMIT = 30;
+
+export const trendingPath = '/gifs/trending';
