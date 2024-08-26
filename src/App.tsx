6698b4a893dac2ae30945608ed83e681da3adbe6
YahiaJabeur <yahiajabeur@gmail.com>
2024-08-26 08:27:47 +0100

    feat: create card grid home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Error } from './components/Error';
+import { Grid } from './components/Grid';
+import { LoadingIndicator } from './components/LoadingIndicator';
+import { LoadMoreButton } from './components/LoadMoreButton';
+import { Modal } from './components/Modal';
+import { useFetchGiphies } from './hooks/useFetchGiphies';
+import { GifType } from './types/GifType';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  const { gifs, loading, error, loadingMore, loadGifs, hasMore } = useFetchGiphies();
+  const [selectedGif, setSelectedGif] = useState<GifType | null>(null);
+
+  const handleCardClick = (gif: GifType) => setSelectedGif(gif);
+  const handleCloseModal = () => setSelectedGif(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      {loading && <LoadingIndicator />}
+      {error && <Error message={error} />}
+      <Grid gifs={gifs} onCardClick={handleCardClick} />
+      {selectedGif && <Modal gif={selectedGif} onClose={handleCloseModal} />}
+      {!loading && hasMore && <LoadMoreButton onClick={loadGifs} isLoading={loadingMore} />}
+    </div>
   );
-}
+};
 
 export default App;
